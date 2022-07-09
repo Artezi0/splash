85ec325fa82548dc32b0c4041e84fd312c209cf7
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2022-07-09 15:49:56 +0700

    Basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,62 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './components/Home'
+import './styles/app.scss'
 
+export default function App() {
+  const datas = [
+    {
+      id: 1,
+      avatar: 'https://avatars.githubusercontent.com/u/87239976?v=4',
+      cover: '#4C3A51',
+      title: 'Ezi',
+      bio: 'Starts with Hello World!'
+    },
+    {
+      id: 2,
+      avatar: 'https://avatars.githubusercontent.com/u/2681894?v=4',
+      cover: '#774360',
+      title: 'Sandhika Galih',
+      bio: 'Hello World!'
+    },
+    {
+      id: 3,
+      avatar: 'https://avatars.githubusercontent.com/u/1332805?v=4',
+      cover: '#B25068',
+      title: 'CraftzDog',
+      bio: 'Hello World!'
+    },
+    {
+      id: 4,
+      avatar: 'https://avatars.githubusercontent.com/u/11247099?v=4',
+      cover: '#E7AB79',
+      title: 'Antfu',
+      bio: 'Hello World!'
+    }
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+          {datas.map(({ id, avatar, cover, title, bio }) => { 
+          return (
+            <Route 
+              key={id}  
+              path={id.toString()} 
+              element={
+                <div className='site'>
+                  <img className='site__avatar' src={avatar} alt='avatar' width={100} height={100} />
+                  <h1 className='site__username'>{title}</h1>
+                  <div className='site__cover' style={{ width: '100px', height: '100px', background: `${cover}` }}></div>
+                  <p className='site__bio'>{bio}</p>
+                </div>
+              } 
+            />
+          )})}
+        </Route>
+      </Routes>
     </div>
   )
 }
-
-export default App
---
+++ src/components/Site.tsx
@@ -0,0 +1,9 @@
+import { dummmy as datas } from "../dummy"
+
+export default function Site() {
+  return (
+    <div className="site">
+      Site
+    </div>
+  ) 
+}
